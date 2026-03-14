50d018e45441a782e852bf272e6c7ad0ac856643
cocacolakire <hasslidstudent@outlook.com>
2026-03-14 14:21:18 +0100

    Change infotext
    Add terminal like appearance for infotext

--- script/script.js
+++ script/script.js
@@ -1,3 +1,36 @@
+function terminalData() {
+    return {
+        lines: [
+            { prompt: 'user@portfolio:~$', cmd: ' whoami' },
+            { prompt: '>', cmd: ' 28 year old engineer with interest in technology' },
+            { prompt: 'user@portfolio:~$', cmd: ' cat bio.txt' },
+            { prompt: '>', cmd: ' I am a civil engineer situated in Sweden, currently working as a Business Intelligence engineer in Karlstad. I have a big interest in technology and I love to learn new stuff. In my spare time I like to play baseball, read, and take on different projects.' },
+        ],
+        displayed: [],
+        cursorLine: 0,
+        init() {
+            this.typeLines(0, 0);
+        },
+        typeLines(lineIdx, charIdx) {
+            if (lineIdx >= this.lines.length) {
+                this.cursorLine = -1;
+                return;
+            }
+            if (charIdx === 0) {
+                this.displayed.push('');
+                this.cursorLine = lineIdx;
+            }
+            const line = this.lines[lineIdx].cmd;
+            if (charIdx < line.length) {
+                this.displayed[lineIdx] += line[charIdx];
+                setTimeout(() => this.typeLines(lineIdx, charIdx + 1), 18);
+            } else {
+                setTimeout(() => this.typeLines(lineIdx + 1, 0), 300);
+            }
+        },
+    }
+}
+
 function carouselData() {
     return {
         IMG_URL: './resources/images/',
